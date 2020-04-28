e5f6328e85eca0c5812b9f5e91a36e7d410c6c22
ChidoriY <y.chidori.266@gmail.com>
2020-04-28 15:33:50 +0900

    おみくじ機能の実装

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,7 +1,23 @@
 'use strict';
 module.exports = (robot) => {
-	robot.hear(/hello>/i, (msg) => {
-		const username = msg.message.user.name;
-		msg.send('Hello, ' + username);
-	});
+  robot.hear(/hello>/i, (msg) => {
+    const user_id = msg.message.user.id;
+    msg.send(`Hello, <@${user_id}>`);
+  });
+  robot.hear(/bye>/i, (msg) => {
+    const user_id = msg.message.user.id;
+    msg.send(`Bye, <@${user_id}>`);
+  });
+  robot.hear(/おみくじ/i, (msg) => {
+    const user_id = msg.message.user.id;
+    const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+    const lot = lots[Math.floor(Math.random() * lots.length)];
+    msg.send(`${lot}, <@${user_id}>`);
+  });
+  robot.hear(/ごはん/i, (msg) => {
+    const user_id = msg.message.user.id;
+    const lots = ['カレー', '餃子', 'ラーメン', 'チャーハン', '焼き肉', 'うどん'];
+    const lot = lots[Math.floor(Math.random() * lots.length)];
+    msg.send(`今日のごはんは ${lot} です！ <@${user_id}>`);
+  });
 };
